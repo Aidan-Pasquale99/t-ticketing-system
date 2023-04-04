c1a760ff36934413d449ea3484e48228f6ba9f4b
Aidan Pasquale <apasquale@temenos.com>
2023-04-04 19:52:01 +0100

    use quotes consistently, add fade animation, update delete modal

--- src/tickets/forms.py
+++ src/tickets/forms.py
@@ -1,60 +1,52 @@
-from datetime import date
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, DateField, SelectField, HiddenField
 from wtforms.validators import InputRequired
 
 
 class CreateTicketForm(FlaskForm):
-    ticket_name = StringField(
+    name = StringField(
         "Ticket Name", 
         validators=[InputRequired()]
     )
 
-    ticket_description = TextAreaField(
-        "Ticket Description"
+    description = TextAreaField(
+        "Ticket Description",
+        validators=[InputRequired()]        
     )
 
-    status = SelectField(u'Ticket Status', choices=[
-        ('to-do', 'To Do'),
-        ('in-progress', 'In Progress'),
-        ('blocked', 'Blocked'),
-        ('completed', 'Completed')
+    status = SelectField(u"Ticket Status", choices=[
+        ("to-do", "To Do"),
+        ("in-progress", "In Progress"),
+        ("blocked", "Blocked"),
+        ("completed", "Completed")
         ],
         validators=[InputRequired()]
         )
 
-    department = SelectField(u'Department', choices=[
-        ('product', 'Product'),
-        ('it', 'IT'),
-        ('sales', 'Sales'),
-        ('hr', 'HR'),
-        ('finance', 'Finance')
+    department = SelectField(u"Department", choices=[
+        ("product", "Product"),
+        ("it", "IT"),
+        ("sales", "Sales"),
+        ("hr", "HR"),
+        ("finance", "Finance")
         ],
         validators=[InputRequired()]
         )
     
-    # TODO: (nice to do) options should be restricted based on the department selected above
-    # Would have to be department table with task types?
-    category = SelectField(u'Category', choices=[
-        ('Development - Coding'),
-        ('Development - Testing'),
-        ('Development - Documenting')
+    # TODO: (nice to do) options should be restricted based on the department selected above - department table?
+    category = SelectField(u"Category", choices=[
+        ("Development - Coding"),
+        ("Development - Testing"),
+        ("Development - Documenting")
         ],
         validators=[InputRequired()]
         )
     
     due_date = DateField(
-        'Due Date', 
+        "Due Date",
         validators=[InputRequired()]
         )
     
-    updated_by = HiddenField('updated_by')
+    updated_by = HiddenField("updated_by")
 
-    updated_date = HiddenField('updated_date')
-
-    # def validate_on_submit(self, extra_validators=None):
-    #     result = super(CreateTicketForm, self).validate()
-    #     if (self.due_date > date.today()):
-    #         return False
-    #     else:
-    #         return result
+    updated_date = HiddenField("updated_date")
