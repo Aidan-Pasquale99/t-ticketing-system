aeb22934ec9b5610fd3170050e0a7123ccc03ee3
Aidan Pasquale <apasquale@temenos.com>
2023-04-04 19:50:09 +0100

    added chart of ticket statuses on home page

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from sqlalchemy import func
+
+from src.tickets.models import Ticket
+from src import db
 
 core_bp = Blueprint("core", __name__)
 
@@ -7,4 +11,18 @@ core_bp = Blueprint("core", __name__)
 @core_bp.route("/")
 @login_required
 def home():
-    return render_template("core/index.html")
+    # get number of tickets
+    number_of_tickets = db.session.query(func.count(Ticket.id)).scalar()
+    if number_of_tickets > 0:
+        # if tickets are present in the database, count the number of tickets in each possible status
+        to_do_tickets = db.session.query(func.count(Ticket.status)).filter(Ticket.status == "to-do").scalar()
+        in_prog_tickets = db.session.query(func.count(Ticket.status)).filter(Ticket.status == "in-progress").scalar()
+        blocked_tickets = db.session.query(func.count(Ticket.status)).filter(Ticket.status == "blocked").scalar()
+        completed_tickets = db.session.query(func.count(Ticket.status)).filter(Ticket.status == "completed").scalar()
+        tickets_exist = True
+        # render home page template, pass in number of tickets in each status to be displayed on the graph
+        return render_template("core/index.html", tickets_exist=tickets_exist, to_do_tickets=to_do_tickets, in_prog_tickets=in_prog_tickets, blocked_tickets=blocked_tickets, completed_tickets=completed_tickets)
+    # if no tickets are present in the database, render the home page without the chart labels and no chart
+    else:
+        tickets_exist = False
+        return render_template("core/index.html", tickets_exist=tickets_exist)
