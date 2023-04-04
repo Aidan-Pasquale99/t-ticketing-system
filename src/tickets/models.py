7f8f896e469056d18ab9ba45f2763c9c9f10227d
Aidan Pasquale <apasquale@temenos.com>
2023-04-04 19:54:30 +0100

    update db model fields for consistent naming conventions

--- src/tickets/models.py
+++ src/tickets/models.py
@@ -1,24 +1,26 @@
 from src import db
 from sqlalchemy import func
 
-# TODO: (nice to do) rename class to Ticket, and add __tablename__ = "tickets"
-class tickets(db.Model):
-    # TODO: (nice to do) clean up fields, remove ticket_ from all ideally
+
+class Ticket(db.Model):
+
+    __tablename__ = "tickets"
+
     id = db.Column(db.Integer, primary_key = True)
-    ticket_name = db.Column(db.String(150))
-    ticket_description = db.Column(db.Text)
+    name = db.Column(db.String(150))
+    description = db.Column(db.Text)
     status = db.Column(db.String(50))
     department = db.Column(db.String(50))
     category = db.Column(db.String(50))
     due_date = db.Column(db.Date)
-    created_by = db.Column(db.ForeignKey('users.email'))
-    updated_by = db.Column(db.ForeignKey('users.email'))
+    created_by = db.Column(db.ForeignKey("users.email"))
+    updated_by = db.Column(db.ForeignKey("users.email"))
     created_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_date = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
-    def __init__(self, ticket_name, ticket_description, status, department, category, due_date, created_by, created_date=func.now()):
-        self.ticket_name = ticket_name
-        self.ticket_description = ticket_description
+    def __init__(self, name, description, status, department, category, due_date, created_by, created_date=func.now()):
+        self.name = name
+        self.description = description
         self.status = status
         self.department = department
         self.category = category
