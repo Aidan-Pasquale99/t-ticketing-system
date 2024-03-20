c3e622c0d1e945fcdfdebcafaad089482c6053aa
Aidan Pasquale <apasquale@temenos.com>
2024-03-20 20:40:38 +0000

    re-add insecure DB access for proof of sqli vuln

--- manage.py
+++ manage.py
@@ -1,13 +1,3 @@
-# from flask.cli import FlaskGroup
-
-# from src import app
-
-# cli = FlaskGroup(app)
-
-
-# if __name__ == "__main__":
-#     cli()
-
 from src import create_app
 
 app = create_app()
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -37,7 +37,7 @@ def login():
     form = LoginForm(request.form)
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password, request.form["password"]):
             login_user(user)
             return redirect(url_for("core.home"))
         else:
